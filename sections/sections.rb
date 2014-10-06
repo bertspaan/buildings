ba18bde9b95f7f0a05500b27f97338fafef58bd3
Bert Spaan <bert@Berts-MacBook-Pro.local>
2014-10-06 14:11:08 +0200

    renamed 'selections' > 'sections'

--- sections/sections.rb
+++ sections/sections.rb
@@ -16,9 +16,11 @@ formats = ["png"] # ["pdf", "png"]
 
 arg_selections = ARGV
 
-r = 2.5 # pixels per meter
+r = 1.8 # pixels per meter
 l = 8000 # longest edge of resulting image in pixels
 a = 3.0/2.0 # aspect ratio of image
+
+convert_to_jpg = false
 
 rgeo_factory = RGeo::Geographic.spherical_factory(:srid => 3785)
 
@@ -69,30 +71,30 @@ selections["features"].each do |selection|
       system tile_cmd % [filename, format, format, (r * dx).round, (r * dy).round, bbox_str]
     }
 
-    puts "Converting #{name} to jpg"
-    s = (l / a).round
-    image = MiniMagick::Image.open("#{export_path}/selections/#{filename}.png")
-    orientation = image[:width] > image[:height] ? :landscape : :portrait
-    puts "\tOrientation: #{orientation}"
-    image.combine_options do |c|
-      c.resize "#{l}x#{l}>"
+    if convert_to_jpg
+      puts "Converting #{name} to jpg"
+      s = (l / a).round
+      image = MiniMagick::Image.open("#{export_path}/selections/#{filename}.png")
+      orientation = image[:width] > image[:height] ? :landscape : :portrait
+      puts "\tOrientation: #{orientation}"
+      image.combine_options do |c|
+        c.resize "#{l}x#{l}>"
 
-      if orientation == :landscape
-        c.crop "#{l}x#{s}+0+0"
-      else
-        c.crop "#{s}x#{l}+0+0"
+        if orientation == :landscape
+          c.crop "#{l}x#{s}+0+0"
+        else
+          c.crop "#{s}x#{l}+0+0"
+        end
+
+        c.gravity "center"
+
       end
+      image.write "#{export_path}/selections/#{filename}_s.png"
 
-      c.gravity "center"
-
+      # Using MiniMagick to convert to JPG leads to 'invalid JPG' errors...
+      system "convert #{export_path}/selections/#{filename}_s.png -colorspace sRGB -quality 99 #{export_path}/selections/#{filename}.jpg"
     end
-    image.write "#{export_path}/selections/#{filename}_s.png"
-
-    # Using MiniMagick to convert to JPG leads to 'invalid JPG' errors...
-    system "convert #{export_path}/selections/#{filename}_s.png -colorspace sRGB -quality 99 #{export_path}/selections/#{filename}.jpg"
-
   end
 end
 
 puts "Done..."
-
